3408dfcd837378a6481779da68a21aa48876bd92
Yılmaz Edis <ylmazedis@gmail.com>
2024-03-15 10:33:38 +0300

    Add unit test

--- PokemonGuideTests/PokemonGuideSwiftUITests.swift
+++ PokemonGuideTests/PokemonGuideSwiftUITests.swift
@@ -1,6 +1,6 @@
 //
-//  PokemonGuideTests.swift
-//  PokemonGuideTests
+//  PokemonGuideSwiftUITests.swift
+//  PokemonGuideSwiftUITests
 //
 //  Created by yilmaz on 13.03.2024.
 //
@@ -8,16 +8,47 @@
 import XCTest
 @testable import PokemonGuide
 
-final class PokemonGuideTests: XCTestCase {
-
+final class PokemonGuideSwiftUITests: XCTestCase {
+    var viewModel: PokemonListViewModel!
+    var mockHttpTask: MockHTTPTask!
+    
+    override func setUp() {
+        super.setUp()
+        mockHttpTask = MockHTTPTask()
+        viewModel = PokemonListViewModel(httpTask: mockHttpTask)
+    }
+    
+    override func tearDown() {
+        viewModel = nil
+        mockHttpTask = nil
+        super.tearDown()
+    }
+    
+    func testFetchPokemonItems_Success() async throws {
+        let expectedItems = [PokemonItem(id: 1, name: "Pikachu", description: "", imageUrl: "")]
+        mockHttpTask.itemsToReturn = expectedItems
+        
+        await viewModel.fetchPokemonItems()
+        
+        XCTAssertEqual(viewModel.items, expectedItems)
+    }
+    
+    func testFetchPokemonItems_Failure() async throws {
+        mockHttpTask.shouldThrowError = true
+        
+        await viewModel.fetchPokemonItems()
+        
+        XCTAssertTrue(viewModel.items.isEmpty)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -25,12 +56,12 @@ final class PokemonGuideTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
