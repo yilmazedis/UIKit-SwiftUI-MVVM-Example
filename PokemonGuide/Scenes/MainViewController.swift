2104ee84f37ec7f2a0fe29aaa97391d978adf602
Yılmaz Edis <ylmazedis@gmail.com>
2024-03-26 00:56:50 +0300

    Refactor MainView

--- PokemonGuide/Scenes/MainViewController.swift
+++ PokemonGuide/Scenes/MainViewController.swift
@@ -29,15 +29,34 @@ final class MainViewController: UIViewController {
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
     
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        updateButtonBorderColor()
+    }
+
+    // UIColor.black.cgColor doesn't effect Buttons layer properties.
+    // So I handle it manually
+    private func updateButtonBorderColor() {
+        if self.traitCollection.userInterfaceStyle == .dark {
+            uiKitButton.layer.borderColor = UIColor.white.cgColor
+            swiftUIButton.layer.borderColor = UIColor.white.cgColor
+        } else {
+            uiKitButton.layer.borderColor = UIColor.black.cgColor
+            swiftUIButton.layer.borderColor = UIColor.black.cgColor
+        }
+    }
+    
     private func prepareView() {
         // These properties can move to IBDesignable to perform into Interface Builde
         uiKitButton.setTitle("UIKit", for: .normal)
+        uiKitButton.setTitleColor(.label, for: .normal)
         uiKitButton.layer.borderColor = UIColor.black.cgColor
         uiKitButton.layer.borderWidth = 1
         uiKitButton.layer.cornerRadius = 5
         uiKitButton.accessibilityIdentifier = "uiKitButton"
         
         swiftUIButton.setTitle("SwiftUI", for: .normal)
+        swiftUIButton.setTitleColor(.label, for: .normal)
         swiftUIButton.layer.borderColor = UIColor.black.cgColor
         swiftUIButton.layer.borderWidth = 1
         swiftUIButton.layer.cornerRadius = 5
