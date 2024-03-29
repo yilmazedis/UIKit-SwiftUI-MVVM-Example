654731413da0010a94e0e6b00e1abb54298f11c2
Yılmaz Edis <ylmazedis@gmail.com>
2024-03-29 03:21:34 +0300

    Remove old style UITableViewDataSource and Add UITableViewDiffableDataSource

--- PokemonGuide/UIKit/Scenes/ListView/ListTableViewController.swift
+++ PokemonGuide/UIKit/Scenes/ListView/ListTableViewController.swift
@@ -8,17 +8,22 @@
 import UIKit
 import Combine
 
+fileprivate typealias ListDataSource = UITableViewDiffableDataSource<ListTableViewController.Section, PokemonItem>
+fileprivate typealias ListSnapshot = NSDiffableDataSourceSnapshot<ListTableViewController.Section, PokemonItem>
+
 final class ListTableViewController: UIViewController {
-    
+
     var viewModel: ListTableViewModel!
     
     @IBOutlet private weak var tableView: UITableView!
     
+    private var dataSource: ListDataSource!
     private var cancellables = Set<AnyCancellable>()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         prepareView()
+        configureDataSource()
         viewModel.fetchPokemonItems()
             .receive(on: DispatchQueue.main)
             .sink { completion in
@@ -29,39 +34,45 @@ final class ListTableViewController: UIViewController {
                     break
                 }
             } receiveValue: { [weak self] items in
-                self?.viewModel.items = items
-                self?.tableView.reloadData()
+                self?.applySnapshot(from: items)
             }
             .store(in: &cancellables)
     }
     
     private func prepareView() {
         tableView.register(UINib(nibName: "ListViewCell", bundle: nil), forCellReuseIdentifier: "ListViewCell")
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
         title = "Pokémon"
+        tableView.delegate = self
+    }
+    
+    private func configureDataSource() {
+        dataSource = ListDataSource(tableView: tableView) { tableView, indexPath, item in
+            /// I already registered ListViewCell, so I dont worry it to put under guard let condition. So it is guarantee not to be crashed.
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath) as! ListViewCell
+            cell.configure(item: item)
+            return cell
+        }
+    }
+    
+    private func applySnapshot(from items: [PokemonItem]) {
+        var snapshot = ListSnapshot()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items)
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
 }
 
-extension ListTableViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.items.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ListViewCell", for: indexPath) as? ListViewCell else {
-            return UITableViewCell()
-        }
-        
-        cell.configure(item: viewModel.items[indexPath.row])
-        
-        return cell
-    }
-    
+extension ListTableViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        viewModel.coordinator.showDetail(with: viewModel.items[indexPath.row])
+        if let selectedItem = dataSource.itemIdentifier(for: indexPath) {
+            viewModel.coordinator.showDetail(with: selectedItem)
+        }
+    }
+}
+
+extension ListTableViewController {
+    // Enum is bydefault hashable
+    fileprivate enum Section {
+        case main
     }
 }
